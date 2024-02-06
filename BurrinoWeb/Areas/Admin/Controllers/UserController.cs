0bc07afe329cbab974806ed2a7e5ed267aeec564
burak <burak@BURAKYIGIT>
2024-02-06 13:31:56 +0300

    -making a few changes

---
+++ BurrinoWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Burrino.DataAccess.Data;
+using Burrino.DataAccess.Repository.IRepository;
+using Burrino.Models;
+using Burrino.Models.ViewModels;
+using Burrino.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace BurrinoWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+         _db = db;
+
+        }
+
+        public IActionResult Index()
+        {
+        
+
+            return View();
+        }
+       
+        
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u=>u.Company).ToList();
+            return Json(new { data = objUserList });
+        }
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+           
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
---
+++ BurrinoWeb/wwwroot/js/user.js
@@ -0,0 +1,32 @@
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/user/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "email", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "company.name", "width": "15%" },
+            { "data": "", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                        <a href="/Admin/Company/Upsert?id=${data}"
+                        class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+					</div>
+                        `
+                },
+                "width": "15%"
+            }
+        ]
+    });
+}
