532680f1884e458796fcefb4361ee1b705387364
burak <burak@BURAKYIGIT>
2024-02-07 01:03:51 +0300

    -product images can be seen in shopping cart

--- BurrinoWeb/Areas/Customer/Controllers/CartController.cs
+++ BurrinoWeb/Areas/Customer/Controllers/CartController.cs
@@ -34,8 +34,10 @@ namespace BurrinoWeb.Areas.Customer.Controllers
                 OrderHeader = new()
 
             };
+            IEnumerable<ProductImage> productImages = _unitOfWork.ProductImage.GetAll();
             foreach (var cart in ShoppingCartVM.ShoppingCartList)
             {
+                cart.Product.ProductImages = productImages.Where(u=>u.ProductId==cart.ProductId).ToList();
                 cart.Price = GetPriceBasedOnQuantity(cart);
                 ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
             }
--- BurrinoWeb/Areas/Customer/Controllers/HomeController.cs
+++ BurrinoWeb/Areas/Customer/Controllers/HomeController.cs
@@ -24,14 +24,14 @@ namespace BurrinoWeb.Areas.Customer.Controllers
         public IActionResult Index()
         {
            
-            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,ProductImages");
             return View(productList);
         }
         public IActionResult Details(int Id)
         {
             ShoppingCart cart = new()
             {
-                Product = _unitOfWork.Product.Get(u => u.Id == Id, includeProperties: "Category"),
+                Product = _unitOfWork.Product.Get(u => u.Id == Id, includeProperties: "Category,ProductImages"),
                 Count = 1,
                 ProductId = Id
             };
